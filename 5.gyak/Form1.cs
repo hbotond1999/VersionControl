08ea5c978cadf5dd010db441dfcae929be42c946
Botond Hegedüs <hbotond1999@gmail.com>
2020-10-07 17:19:11 +0200

    #4-5

--- 5.gyak/Form1.cs
+++ 5.gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5.gyak.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,28 @@ using System.Windows.Forms;
 
 namespace _5.gyak
 {
+   
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem {Index="OTP", Volume=10 });
+            Portfolio.Add(new PortfolioItem { Index="ZWACK", Volume=10});
+            Portfolio.Add(new PortfolioItem { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
