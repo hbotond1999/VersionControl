ed103f8ab18f8b781c81bc9c5916f8cb7232d418
Botond Hegedüs <hbotond1999@gmail.com>
2020-10-15 16:01:32 +0200

    5

--- 6.gyak/Form1.cs
+++ 6.gyak/Form1.cs
@@ -1,14 +1,9 @@
 ﻿using _6.gyak.Entities;
 using _6.gyak.MnbServiceReference;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace _6.gyak
 {
@@ -17,18 +12,49 @@ namespace _6.gyak
         MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
         GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
         {
-            currencyNames = "Valuta",
+            currencyNames = "EUR",
             startDate = "2020-01-01",
-            endDate= "2020-06-30"
+            endDate = "2020-06-30"
         };
 
         BindingList<RateData> rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+            XML();
+
+            dataGridView1.DataSource = rates;
+           
+
+
+
+        }
+        private void XML()
+        {
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
-            dataGridView1.DataSource = rates;
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
+
+
     }
 }
