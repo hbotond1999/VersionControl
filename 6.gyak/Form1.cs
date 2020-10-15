336b6fd8e56601eb64b5f4a8380ccbd4a0278e42
Botond Hegedüs <hbotond1999@gmail.com>
2020-10-15 15:24:52 +0200

    2

--- 6.gyak/Form1.cs
+++ 6.gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6.gyak.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,19 @@ namespace _6.gyak
 {
     public partial class Form1 : Form
     {
+        MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+        GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
+        {
+            currencyNames = "Valuta",
+            startDate = "2020-01-01",
+            endDate= "2020-06-30"
+        };
+        
         public Form1()
         {
             InitializeComponent();
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
