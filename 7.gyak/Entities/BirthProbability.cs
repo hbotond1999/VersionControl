780ab5feadc3516c172568236a6cdad19189ae86
Botond Hegedüs <hbotond1999@gmail.com>
2020-10-22 14:16:17 +0200

    3

---
+++ 7.gyak/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.gyak.Entities
+{
+    class BirthProbability
+    {
+        public int Kor { get; set; }
+        public int GyermekekSzáma { get; set; }
+        public double P { get; set; }
+
+    }
+}
---
+++ 7.gyak/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.gyak.Entities
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+       
+        public Gender Gender { get; set; }
+        public int NumberOfChildren { get; set; }
+        public bool Isalive { get; set; }
+
+    }
+}
