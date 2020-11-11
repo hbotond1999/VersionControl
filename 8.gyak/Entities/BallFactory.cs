7ea03f761ee5799ed4911f1b1f8a45551a7b0f71
Botond Hegedüs <hbotond1999@gmail.com>
2020-11-11 13:35:56 +0100

    3

---
+++ 8.gyak/Entities/BallFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.gyak.Entities
+{
+    class BallFactory
+    {
+
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+
+    }
+}
