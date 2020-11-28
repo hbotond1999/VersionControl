a05c7f086089f018c0f9a37bf8b38d6535ad0d7c
Botond Hegedüs <hbotond1999@gmail.com>
2020-11-28 12:10:11 +0100

    2

--- 9.gyak/Form1.cs
+++ 9.gyak/Form1.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace _9.gyak
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+            
         }
     }
 }
