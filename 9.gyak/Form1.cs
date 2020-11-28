90a441824143e4e3f707e55b52ff6d45b63090c7
Botond Hegedüs <hbotond1999@gmail.com>
2020-11-28 12:19:14 +0100

    4

--- 9.gyak/Form1.cs
+++ 9.gyak/Form1.cs
@@ -7,6 +7,10 @@ namespace _9.gyak
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
@@ -15,6 +19,13 @@ namespace _9.gyak
             this.Controls.Add(ga);
             //gc.AddPlayer();
             //gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            };
+            gc.Start();
+
+
         }
     }
 }
